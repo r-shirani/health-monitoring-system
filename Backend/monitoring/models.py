920e3076436a4070dd90d363dcd124ff5b2d399b
r-shirani <rsh96785@gmail.com>
2026-05-03 13:17:24 +0330

    add Device and VitalSign models with migration

--- Backend/monitoring/models.py
+++ Backend/monitoring/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Device(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    device_id = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.device_id} {self.user.username}"
+
+class VitalSign(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    heart_rate = models.FloatField(null=True, blank=True)
+    oxygen_level = models.FloatField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.device.device_id} {self.heart_rate} {self.oxygen_level} {self.timestamp}"
