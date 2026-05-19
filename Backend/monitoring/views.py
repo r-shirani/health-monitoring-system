b72fe2582d49445a1e9229cffc867428167a2109
r-shirani <rsh96785@gmail.com>
2026-05-19 17:19:00 +0330

    add device filtering to vital signs API

--- Backend/monitoring/views.py
+++ Backend/monitoring/views.py
@@ -10,3 +10,11 @@ class DeviceViewSet(viewsets.ModelViewSet):
 class VitalSignViewSet(viewsets.ModelViewSet):
     queryset = VitalSign.objects.all()
     serializer_class = VitalSignSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        device_id = self.request.query_params.get('device')
+        if device_id is not None:
+            queryset = queryset.filter(device_id=device_id)
+
+        return queryset
