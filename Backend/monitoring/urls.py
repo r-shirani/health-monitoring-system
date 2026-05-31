37791d0f451bbf56c0acf01facc6be1bfc172b55
r-shirani <rsh96785@gmail.com>
2026-05-31 20:17:37 +0330

    add login and logout urls

--- Backend/monitoring/urls.py
+++ Backend/monitoring/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import DeviceViewSet, VitalSignViewSet
+from .views import DeviceViewSet, VitalSignViewSet, dashboard
+from django.contrib.auth import views as auth_views
+
 
 router = DefaultRouter()
 
@@ -9,4 +11,7 @@ router.register(r'vitals', VitalSignViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(template_name='monitoring/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
