b95818403a80a5de7b77d4e535eaece8c92bcd96
r-shirani <rsh96785@gmail.com>
2026-06-24 21:29:26 +0330

    config: configure SMTP email backend settings

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -137,3 +137,14 @@ REST_FRAMEWORK = {
                 'rest_framework.permissions.IsAuthenticated',
     ),
 }
+
+# ==============================================================================
+# EMAIL CONFIGURATION (EMERGENCY ALERTS)
+# ==============================================================================
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'health.monitoring.sys2026@gmail.com'
+EMAIL_HOST_PASSWORD = 'srlycfukyunsicgy'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
