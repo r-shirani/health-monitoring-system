1772760e322bc1564cbac8e62418b18c1908f3c9
r-shirani <rsh96785@gmail.com>
2026-06-24 21:30:44 +0330

    feat: add last_email_sent field to Device model with migration

--- Backend/monitoring/models.py
+++ Backend/monitoring/models.py
@@ -8,6 +8,7 @@ class Device(models.Model):
     is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     emergency_email = models.EmailField(blank=True, null=True)
+    last_email_sent = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.device_id} {self.user.username}"
